d651acd21d4a4fc668c4b17f9c3aececbaf83b08
sinesio <cesar.eav@gmail.com>
2022-11-17 19:19:03 -0300

    Mejoras diseño menú mobile

--- resources/js/views/Header.vue
+++ resources/js/views/Header.vue
@@ -3,9 +3,9 @@
 
     
 <nav class="px-2 bg-red-500 text-gray-200 shadow-lg p-3 ">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
+  <div class="container flex flex-wrap md:justify-start justify-between items-center mx-auto">
     <a href="/" class="flex items-center">
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">La Brújula</span>
+        <span class="self-center text-xl font-semibold whitespace-nowrap md:hover:text-gray-100 dark:text-white">La Brújula</span>
     </a>
     <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex justify-center items-center ml-3 text-white rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500" aria-controls="mobile-menu-2" aria-expanded="false">
       <span class="sr-only">Open main menu</span>
@@ -13,13 +13,11 @@
     </button>
     <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
       <ul class="Menu-Escritorio flex flex-col p-4  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-
-
         <li>
             <button 
             id="dropdownNavbarLink" 
             data-dropdown-toggle="dropdownNavbar" 
-            class="CERROS flex justify-between  text-sm items-left py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+            class="CERROS flex justify-between  text-sm items-left py-2 pr-4 pl-3 w-full font-medium text-white rounded hover:bg-red-600 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 md:w-auto dark:text-red-600 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
              Cerros 
             <svg class="ml-1 w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd">
             </path>
@@ -135,6 +133,9 @@
     margin-left: 8px;
     padding: 2px;
     /* background-color: black; */
+  }
+  .Sub-menu:hover{
+    color:  rgb(231, 37, 12);
 
   }
 
