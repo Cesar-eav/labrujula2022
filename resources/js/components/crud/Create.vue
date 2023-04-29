bbef645b68aaf6c64167674662686364c6c78990
sinesio <cesar.eav@gmail.com>
2023-04-28 22:13:43 -0400

    1er comit.

--- resources/js/components/crud/Create.vue
+++ resources/js/components/crud/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center m-3">
-    <h1 class="text-2xl text-center mb-3">AGREGAR PUNTO</h1>
+    <h1 class="text-2xl text-center mb-3">AGREGAR PUNTOs</h1>
 
 <!-- PUNTOS CRUD -->
 <div class="flex justify-center">
